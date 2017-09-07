e529943586ba65a3a9a9e6e9fd4c54a3f4ec32c0
jerrywiston <dreamfantasy0@gmail.com>
2017-09-07 23:55:06 +0800

    Add Reparameterize GLO

--- rglo_tensorflow.py
+++ rglo_tensorflow.py
@@ -28,22 +28,23 @@ def plot_x(id, type, samp):
     plt.close(fig)
 
 def plot_z(z, dim1, dim2, id, samp):
-    x = z[0:1000, dim1]
-    y = z[0:1000, dim2]
+	ax_lim = 2.0#1.2
+	x = z[0:1000, dim1]
+	y = z[0:1000, dim2]
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_aspect('equal')
-    ax.set_xlim([-1.2,1.2])
-    ax.set_ylim([-1.2,1.2])
-    ax.plot(x,y,'b.')
-    ax.plot(samp[:,dim1], samp[:,dim2], 'ro')
-    
-    an = np.linspace(0, 2*np.pi, 100)
-    ax.plot(np.cos(an), np.sin(an), 'r')
-    
-    plt.savefig('out/{}_dist.png'.format(str(id).zfill(4)), bbox_inches='tight')
-    plt.close(fig)
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.set_aspect('equal')
+	ax.set_xlim([-ax_lim, ax_lim])
+	ax.set_ylim([-ax_lim, ax_lim])
+	ax.plot(x,y,'b.')
+	ax.plot(samp[:,dim1], samp[:,dim2], 'ro')
+
+	an = np.linspace(0, 2*np.pi, 100)
+	ax.plot(np.cos(an), np.sin(an), 'r')
+
+	plt.savefig('out/{}_dist.png'.format(str(id).zfill(4)), bbox_inches='tight')
+	plt.close(fig)
 
 def xavier_init(size):
     in_dim = size[0]
@@ -71,31 +72,51 @@ def latent_rescale(z):
 def train_latent(grad, id_list, z_train, rate):
     for i in range(id_list.shape[0]):
         z_update = z_train[id_list[i]] - rate * grad[i]
-        z_train[id_list[i]] = latent_rescale(z_update)
-    
+        #z_train[id_list[i]] = latent_rescale(z_update)
+        z_train[id_list[i]] = z_update
+
 #================================= Data & Parameter =================================
 #Some parameter
-latent_size = 8
+latent_size = 256
+repar_size = 100
 batch_size = 256
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-x_train = mnist.train.images
-z_train = np.random.normal(0., 0.5, [x_train.shape[0], latent_size])
+x_train = mnist.train.images[0:10000]
+k_train = np.random.normal(0., 0.01, [x_train.shape[0], latent_size])
 
 #================================= Network model =================================
 #Placeholder
-z_ = tf.placeholder(tf.float32, shape=[None, latent_size])
+k_ = tf.placeholder(tf.float32, shape=[None, latent_size])
+z_ = tf.placeholder(tf.float32, shape=[None, repar_size])
 x_ = tf.placeholder(tf.float32, shape=[None, 784])
 
 #Model Variable
-W_g1 = tf.Variable(xavier_init([latent_size,16]))
-b_g1 = tf.Variable(tf.zeros(shape=[16]))
-W_g2 = tf.Variable(xavier_init([16,128]))
+W_r1 = tf.Variable(xavier_init([latent_size, 128]))
+b_r1 = tf.Variable(tf.zeros(shape=[128]))
+W_mu = tf.Variable(xavier_init([128, repar_size]))
+b_mu = tf.Variable(tf.zeros(shape=[repar_size]))
+W_sigma = tf.Variable(xavier_init([128, repar_size]))
+b_sigma = tf.Variable(tf.zeros(shape=[repar_size]))
+
+W_g1 = tf.Variable(xavier_init([repar_size,32]))
+b_g1 = tf.Variable(tf.zeros(shape=[32]))
+W_g2 = tf.Variable(xavier_init([32,128]))
 b_g2 = tf.Variable(tf.zeros(shape=[128]))
 W_g3 = tf.Variable(xavier_init([128,784]))
 b_g3 = tf.Variable(tf.zeros(shape=[784]))
 
 #Model Implement
+def Reparameter(k):
+	h_r1 = tf.nn.relu(tf.matmul(k, W_r1) + b_r1)
+	z_mu = tf.matmul(h_r1, W_mu) + b_mu
+	z_logvar = tf.matmul(h_r1, W_sigma) + b_sigma
+	return z_mu, z_logvar
+
+def SampleZ(mu, log_var):
+	eps = tf.random_normal(shape=tf.shape(mu))
+	return mu + tf.exp(log_var / 2) * eps
+
 def Generator(z):
     h_g1 = tf.nn.relu(tf.matmul(z, W_g1) + b_g1)
     h_g2 = tf.nn.relu(tf.matmul(h_g1, W_g2) + b_g2)
@@ -103,15 +124,19 @@ def Generator(z):
     x_re_prob = tf.nn.sigmoid(x_re_logit)
     return x_re_logit, x_re_prob
 
-x_logits, x_prob = Generator(z_)
+z_mu, z_logvar = Reparameter(k_)
+z_sample = SampleZ(z_mu, z_logvar)
+x_logits, x_prob = Generator(z_sample)
+_, x_sample = Generator(z_)
 
 #Loss and optimizer
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(x_prob - x_), reduction_indices=[1]))
-#loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logits, labels=x_))
+#recon_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=x_prob, labels=x_), 1)
+recon_loss = tf.reduce_mean(tf.reduce_sum(tf.square(x_prob - x_), reduction_indices=[1]))
+kl_loss = 0.5 * tf.reduce_sum(tf.exp(z_logvar) + z_mu**2 - 1. - z_logvar, 1)
+loss = tf.reduce_mean(recon_loss + kl_loss)
 
-z_gradients = tf.gradients(loss, z_)
+z_gradients = tf.gradients(loss, k_)
 solver = tf.train.AdamOptimizer().minimize(loss)
-#solver = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
 
 #Build model
 sess = tf.Session()
@@ -124,26 +149,27 @@ if not os.path.exists('out/'):
 i=0
 for it in range(50001):
     #Train weight & latent
-    x_batch, z_batch, id_batch = mnist_next_batch(x_train, z_train, batch_size)
-    _, grad = sess.run([solver, z_gradients], feed_dict={x_: x_batch, z_: z_batch})
+    x_batch, k_batch, id_batch = mnist_next_batch(x_train, k_train, batch_size)
+    _, grad = sess.run([solver, z_gradients], feed_dict={x_: x_batch, k_: k_batch})
     grad_np = np.asarray(grad[0])
-    train_latent(grad_np, id_batch, z_train, 1.)
+    train_latent(grad_np, id_batch, k_train, 0.1)
 
     #Print message
     if it % 100 == 0:
-        loss_ = sess.run(loss, feed_dict={x_: x_batch, z_: z_batch})
+        loss_ = sess.run(loss, feed_dict={x_: x_batch, k_: k_batch})
         print('Iter: {}, loss: {}'.format(it, loss_))
         
         #Reconstruct x test
-        _, z_test, _ = mnist_next_batch(x_train, z_train, 16)
-        samples_re = sess.run(x_prob, feed_dict={z_: z_test})
+        _, k_test, _ = mnist_next_batch(x_train, k_train, 16)
+        samples_re = sess.run(x_prob, feed_dict={k_: k_test})
+        plot_x(i,'recon', samples_re)
 
         #Random sample z test
-        z_samp = np.random.normal(0., 0.3, [16, latent_size])
-        samples_samp = sess.run(x_prob, feed_dict={z_: z_samp})
+        z_samp = np.random.normal(0., 1., [16, repar_size])
+        samples_samp = sess.run(x_sample, feed_dict={z_: z_samp})
+        plot_x(i,'samp', samples_samp)
         
         #Ouput figure
-        plot_x(i,'recon', samples_re)
-        plot_x(i,'samp', samples_samp)
-        plot_z(z_train, 0, 1, i, z_samp)
+        z_plot = sess.run(z_sample, feed_dict={k_: k_train[0:1000]})
+        plot_z(z_plot, 0, 1, i, z_samp)
         i += 1
